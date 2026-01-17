f202d8c98bd3827a828cb63647b21e6aab52ed02
Yenny-Durango-Dev <yennycarolinadurango@gmail.com>
2026-01-17 17:08:47 -0500

    primera modificacion

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-purple-600 text-white text-3xl font-bold">
+      TAILWIND FUNCIONA, POR FIN
+    </div>
+  );
+}
